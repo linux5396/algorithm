1bbaaf8e125d3c49eaf727e885856d3861685aa5
linux5396 <929159338@qq.com>
2019-09-20 13:05:55 +0800

    回溯法之机器人最大路径检索read

--- src/main/java/com/linxu/algorithm/bydate/date190919/RobotRunRange.java
+++ src/main/java/com/linxu/algorithm/bydate/date190919/RobotRunRange.java
@@ -1,5 +1,7 @@
 package com.linxu.algorithm.bydate.date190919;
 
+import com.linxu.algorithm.Recommend;
+
 /**
  * @author linxu
  * @date 2019/9/19
@@ -11,19 +13,32 @@ package com.linxu.algorithm.bydate.date190919;
  * 数位之和（例如(35,37)数位之和为3+5+3+7=18）
  */
 public class RobotRunRange {
+    @Recommend
     public static int canMoving(int[][] matrix, int k) {
         //solve special case
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
-        //normal case
+        //base count
+        //所有结点都是在该节点的基础上自增
         int count = 0;
         //construct a boolean array to record the visit.
         boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        if (check(matrix, visited, k, 0, 0)) {
+        count = canMove(matrix, visited, k, 0, 0, count);
+        return count;
+    }
 
+    private static int canMove(int[][] matrix, boolean[][] visited, int k, int row, int col, int count) {
+        //solve special case
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        if (check(matrix, visited, k, row, col)) {
+            visited[row][col] = true;
+            count = 1 + canMove(matrix, visited, k, row + 1, col, count)
+                    + canMove(matrix, visited, k, row - 1, col, count)
+                    + canMove(matrix, visited, k, row, col + 1, count)
+                    + canMove(matrix, visited, k, row, col - 1, count);
         }
         return count;
     }
@@ -50,5 +65,14 @@ public class RobotRunRange {
             canMove = true;
         }
         return canMove;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 1, 1, 4},
+                {2, 3, 78, 79},
+                {4, 80, 6, 7}
+        };
+        System.out.println(canMoving(matrix, 3));
     }
 }
