dfde8ebefa4efb9601bdd2c135c70be306184230
linux5396 <929159338@qq.com>
2019-10-03 16:35:17 +0800

    进制思想之钻石砝码

---
+++ src/main/java/com/linxu/algorithm/bydate/date191003/DiamondProblem.java
@@ -0,0 +1,15 @@
+package com.linxu.algorithm.bydate.date191003;
+
+/**
+ * @author linxu
+ * @date 2019/10/3
+ * <tip>take care of yourself.everything is no in vain.</tip>
+ * 腾讯面试问题：
+ * 一个商人到市场上买钻石，这批钻石的质量都是41g以下，但是商人只有四个砝码，请问他该带哪四个砝码？
+ */
+public class DiamondProblem {
+    //思想,采用X进制思想
+    //41g以下且只能带四个砝码，则最大为40G
+    //假设砝码都用上，则为1111表示40，那么即分别为 27 9 3 1
+    //TODO 进制思想总结：包括幂次计算、大数加法、多重背包、钻石砝码等
+}
