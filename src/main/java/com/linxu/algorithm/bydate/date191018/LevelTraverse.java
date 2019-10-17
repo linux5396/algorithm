8bc7ef48e6594f23ab535fb334364c05ccb4b500
linux5396 <929159338@qq.com>
2019-10-17 21:00:09 +0800

    二叉树的层次遍历

--- src/main/java/com/linxu/algorithm/bydate/date191018/LevelTraverse.java
+++ src/main/java/com/linxu/algorithm/bydate/date191018/LevelTraverse.java
@@ -12,6 +12,17 @@ import java.util.Queue;
  * 二叉树的层次遍历
  */
 public class LevelTraverse {
+    /**
+     * 常量换行符
+     */
+    private static final String ENTER = "\n";
+
+    /**
+     * 不分行的层次遍历；
+     *
+     * @param root
+     * @return
+     */
     public ArrayList<Integer> print(Node root) {
         if (root != null) {
             ArrayList<Integer> arrayList = new ArrayList<>();
@@ -33,9 +44,66 @@ public class LevelTraverse {
         return new ArrayList<>();
     }
 
+    /**
+     * 使用两个变量控制换行；一个为当前行还没打印的节点数；一个为下一行的节点数。
+     * @param root
+     */
+    public void printWithEnter(Node root) {
+        if (root != null) {
+            Queue<Node> queue = new ArrayDeque<>();
+            ((ArrayDeque<Node>) queue).addFirst(root);
+            int rareNode = 1;
+            int nextLevelNodes = 0;
+            while (!queue.isEmpty()) {
+                Node headNode = ((ArrayDeque<Node>) queue).pollFirst();
+                rareNode--;
+                System.out.print(headNode.val+",");
+                if (headNode.left != null) {
+                    ((ArrayDeque<Node>) queue).addLast(headNode.left);
+                    nextLevelNodes++;
+                }
+                if (headNode.right != null) {
+                    ((ArrayDeque<Node>) queue).addLast(headNode.right);
+                    nextLevelNodes++;
+                }
+                if (rareNode == 0) {
+                    System.out.print(ENTER);
+                    rareNode = nextLevelNodes;
+                    nextLevelNodes = 0;
+                }
+            }
+        }
+    }
+
     static class Node {
         int val;
         Node left;
         Node right;
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node();
+        root.val=8;
+        Node n1=new Node();
+        n1.val=6;
+        Node n2=new Node();
+        n2.val=10;
+        Node n3=new Node();
+        n3.val=5;
+        Node n4=new Node();
+        n4.val=7;
+        Node n5=new Node();
+        n5.val=9;
+        Node n6=new Node();
+        n6.val=11;
+
+        root.left=n1;
+        root.right=n2;
+        n1.left=n3;
+        n1.right=n4;
+        n2.left=n5;
+        n2.right=n6;
+
+        new LevelTraverse().printWithEnter(root);
     }
 }
