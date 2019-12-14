cb0c2b3a42fccd082a29558f2ffec69a9f0195b5
linux5396 <929159338@qq.com>
2019-12-15 01:42:11 +0800

    三色排序

---
+++ src/main/java/com/linxu/algorithm/bydate/date191215/ColorSort.java
@@ -0,0 +1,61 @@
+package com.linxu.algorithm.bydate.date191215;
+
+import com.linxu.algorithm.utils.GenerationUtil;
+
+/**
+ * @author linxu
+ * @date 2019/12/15
+ * <tip>take care of yourself.everything is no in vain.</tip>
+ * 问题：三色排序，用0 1 2代表
+ */
+public class ColorSort {
+
+    public static int[] colorSort(int[] colors) {
+        if (colors == null || colors.length == 0) {
+            return null;
+        }
+        //normal 
+        int low = 0;
+        int high = colors.length - 1;
+
+        for (int i = 0; i < high; i++) {
+            if (colors[i] == 0) {
+                GenerationUtil.swap(colors, i, low);
+                ++low;
+            } else if (colors[i] == 2) {
+                GenerationUtil.swap(colors, i, high);
+                high--;
+                i--;
+            }
+        }
+        return colors;
+    }
+
+    public static int[] myColorSort(int[] colors) {
+        if (colors == null || colors.length == 0) {
+            return null;
+        }
+        int low = 0;
+        int middle = 0;
+        int high = colors.length - 1;
+        //限定
+        while (middle < high) {
+            if (colors[middle] == 0) {
+                GenerationUtil.swap(colors, middle, low);
+                low++;
+                middle++;
+            } else if (colors[middle] == 2) {
+                GenerationUtil.swap(colors, middle, high);
+                high--;
+            } else {
+                middle++;
+            }
+        }
+        return colors;
+    }
+
+    public static void main(String[] args) {
+        int[] c = {0, 0, 2, 0, 1, 2, 1, 2,1};
+        GenerationUtil.print(myColorSort(c), false);
+    }
+}
