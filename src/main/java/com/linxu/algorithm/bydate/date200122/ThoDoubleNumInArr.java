a358c04399881125c8a8fa24e7e61b7cab772637
linux5396 <929159338@qq.com>
2020-01-22 00:05:24 +0800

    找出数组中两个只出现一次的数字，利用异或加位分组

---
+++ src/main/java/com/linxu/algorithm/bydate/date200122/ThoDoubleNumInArr.java
@@ -0,0 +1,61 @@
+package com.linxu.algorithm.bydate.date200122;
+
+import com.linxu.algorithm.CostSpace;
+import com.linxu.algorithm.CostTime;
+
+/**
+ * @author linxu
+ * @date 2020/1/21
+ * <tip>take care of yourself.everything is no in vain.</tip>
+ * 问题：在数组中有两个数字出现了1次，其余都出现了2次，请在
+ * O(N)的时间复杂度与O(1)的空间复杂度内找出这两个数字
+ */
+public class ThoDoubleNumInArr {
+    @CostSpace("1")
+    @CostTime("N")
+    public static int[] find(int[] array) {
+        //special case solve.
+        if (array == null || (array.length & 1) == 1) {
+            return new int[0];
+        }
+        int[] retVals = new int[2];
+        int xorSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            xorSum ^= array[i];
+        }
+        int boundIdx = findTheBoundBitIdx(xorSum);
+        for (int i = 0; i < array.length; i++) {
+            if (isBound(array[i], boundIdx)) {
+                retVals[0] ^= array[i];
+            } else {
+                retVals[1] ^= array[i];
+            }
+        }
+        return retVals;
+    }
+
+    /**
+     * 查找异或后结果的第一个1，用来分组
+     */
+    private static int findTheBoundBitIdx(int num) {
+        int indexOfBit = 0;
+        while ((num & 1) == 0 && indexOfBit < 32) {
+            num >>= 1;
+            indexOfBit++;
+        }
+        return indexOfBit;
+    }
+
+    /**
+     * 判断是在哪一组
+     *
+     * @param num
+     * @param boundIdx
+     * @return
+     */
+    private static boolean isBound(int num, int boundIdx) {
+        num >>= boundIdx;
+        return (num & 1) == 1;
+    }
+
+}
