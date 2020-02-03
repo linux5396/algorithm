d6f78f9a3c83e6d595095083353bf90fc45d4d90
linux5396 <929159338@qq.com>
2020-02-04 02:02:26 +0800

    贪心、桶排任务调度器

---
+++ src/main/java/com/linxu/algorithm/hot100/greedy/TaskDispatcher.java
@@ -0,0 +1,49 @@
+package com.linxu.algorithm.hot100.greedy;
+
+import java.util.*;
+
+/**
+ * @author linxu
+ * @date 2020/2/3
+ * <tip>take care of yourself.everything is no in vain.</tip>
+ * 给定一个用字符数组表示的 CPU 需要执行的任务列表。其中包含使用大写的 A - Z 字母表示的26 种不同种类的任务。
+ * 任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务，或者在待命状态。
+ * 然而，两个相同种类的任务之间必须有长度为 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+ * <p>
+ * 你需要计算完成所有任务所需要的最短时间。
+ * 任务的总个数为 [1, 10000]。
+ * n 的取值范围为 [0, 100]。
+ * <p>
+ * 输入: tasks = ["A","A","A","B","B","B"], n = 2
+ * 输出: 8
+ * 执行顺序: A -> B -> (待命) -> A -> B -> (待命) -> A -> B.
+ */
+public class TaskDispatcher {
+    public int leastInterval(char[] tasks, int n) {
+        if (tasks == null || (tasks.length == 0) || tasks.length > 10000) {
+            return 0;
+        }
+        if (n < 0 || n > 100 || n == 0) {
+            return tasks.length;
+        }
+        Map<Character, Integer> map = new HashMap<>(26);
+        int buckets = 0;
+        int mulMaxEqualsNumTask = 0;
+        for (Character ch : tasks) {
+            int count = map.getOrDefault(ch, 0);
+            map.put(ch, ++count);
+            //最大的决定buckets
+            buckets = Math.max(buckets, count);
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == buckets)
+                mulMaxEqualsNumTask++;
+        }
+        return Math.max((n + 1) * buckets + mulMaxEqualsNumTask,tasks.length );
+    }
+
+    public static void main(String[] args) {
+        char[] tasks = "AAAB".toCharArray();
+        System.out.println(new TaskDispatcher().leastInterval(tasks, 1));
+    }
+}
