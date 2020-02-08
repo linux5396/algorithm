5394b6f00ecaf36e3d91f40b126463f9004f32a4
linux5396 <929159338@qq.com>
2020-02-08 21:52:35 +0800

    二叉树的路径和为targetVal的路径

---
+++ src/main/java/com/linxu/algorithm/hot100/backtrack/Parenthesis.java
@@ -0,0 +1,41 @@
+package com.linxu.algorithm.hot100.backtrack;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author linxu
+ * @date 2020/2/8
+ * <tip>take care of yourself.everything is no in vain.</tip>
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ * <p>
+ * 例如，给出 n = 3，生成结果为：
+ * <p>
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ */
+public class Parenthesis {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new LinkedList<>();
+        track(list, "", 0, 0, n);
+        return list;
+    }
+
+    private void track(List<String> list, String cur, int open, int close, int max) {
+        if (cur.length() == max<<1) {
+            list.add(cur);
+            return;
+        }
+        if (open < max) {
+            track(list, cur + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            track(list, cur + ")", open, close + 1, max);
+        }
+    }
+}
