1f2f3455c58544bed33b74e527cfc1eed13f7606
antonlin <antonlin@tencent.com>
2020-05-15 17:55:48 +0800

    create new node

---
+++ src/main/java/comeon/binarytree/TreeNode.java
@@ -0,0 +1,15 @@
+package comeon.binarytree;
+
+public class TreeNode {
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode() {
+    }
+
+    public TreeNode left;
+    public TreeNode right;
+    public int val;
+}
